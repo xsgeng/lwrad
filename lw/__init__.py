ec4992db800576c35386f7b15f2b5b6fcaa0c0ff
xsgeng <xsgeng@siom.ac.cn>
2022-11-10 15:11:59 +0800

    use nx ny nz

--- lw/__init__.py
+++ lw/__init__.py
@@ -18,16 +18,18 @@ def get_lw_RE(x, y, z, ux, uy, uz, t, n):
         长度为1的方向矢量
     '''
 
-    n_norm = np.sqrt(n[0]**2 + n[1]**2 + n[2]**2)
-    if isinstance(n, tuple):
-        n = list(n)
+    nx = n[0]
+    ny = n[1]
+    nz = n[2]
+    n_norm = np.sqrt(nx**2 + ny**2 + nz**2)
     if n_norm != 1:
-        n[0] /= n_norm
-        n[1] /= n_norm
-        n[2] /= n_norm
+        nx /= n_norm
+        ny /= n_norm
+        nz /= n_norm
 
 
-    t_ret = t - (n[0]*x + n[1]*y + n[2]*z) / c
+    t_ret = t - (nx*x + ny*y + nz*z) / c
+
     inv_gamma = 1 / np.sqrt(ux**2 + uy**2 + uz**2 + 1)
     betax = ux * inv_gamma
     betay = uy * inv_gamma
@@ -37,14 +39,14 @@ def get_lw_RE(x, y, z, ux, uy, uz, t, n):
     ay = np.concatenate(([0], (betay[2:] - betay[:-2])/(t[2:] - t[:-2]), [0]))
     az = np.concatenate(([0], (betaz[2:] - betaz[:-2])/(t[2:] - t[:-2]), [0]))
 
-    n_dot_a = n[0]*ax + n[1]*ay + n[2]*az
-    n_dot_beta = n[0]*betax + n[1]*betay + n[2]*betaz
+    n_dot_a = nx*ax + ny*ay + nz*az
+    n_dot_beta = nx*betax + ny*betay + nz*betaz
     factor = 1 / (1 - n_dot_beta)**3
     factor *= e / (4*pi*epsilon_0*c)
 
-    REx = (n_dot_a*(n[0] - betax) + (n_dot_beta - 1) * ax) * factor
-    REy = (n_dot_a*(n[1] - betay) + (n_dot_beta - 1) * ay) * factor
-    REz = (n_dot_a*(n[2] - betaz) + (n_dot_beta - 1) * az) * factor
+    REx = (n_dot_a*(nx - betax) + (n_dot_beta - 1) * ax) * factor
+    REy = (n_dot_a*(ny - betay) + (n_dot_beta - 1) * ay) * factor
+    REz = (n_dot_a*(nz - betaz) + (n_dot_beta - 1) * az) * factor
     return t_ret, REx, REy, REz
 
 
