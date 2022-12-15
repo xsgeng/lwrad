c6e989f4dd4b327727b0e40175c750f97a9b23ab
gengxs <xsgeng@siom.ac.cn>
2022-12-15 15:40:42 +0800

    多粒子计算

--- lw/__init__.py
+++ lw/__init__.py
@@ -1,7 +1,6 @@
 import numpy as np
 from scipy.constants import pi, m_e, e, c, alpha, hbar, epsilon_0, mu_0
 from numba import njit, prange
-from .gpu import get_RE_spectrum_d
 from warnings import warn
 
 def get_lw_RE(x, y, z, ux, uy, uz, t, n):
@@ -95,6 +94,8 @@ def get_RE_spectrum(RE, t_ret, omega_axis):
     omega_axis : ndarray
         频谱的频率轴
     '''
+    assert RE.ndim == 1 and t_ret.ndim == 1 and omega_axis.ndim == 1
+    
     nomega = len(omega_axis)
     RE_ft = np.zeros(nomega, dtype=np.complex128)
     norm = 1 / np.sqrt(c*mu_0) / np.sqrt(2*pi)
@@ -108,17 +109,50 @@ def get_RE_spectrum(RE, t_ret, omega_axis):
     # normalize
     return RE_ft
 
+@njit(parallel=True)
+def get_RE_spectrum_2d(RE, t_ret, omega_axis):
+    '''
+    计算lw谱，不使用FFT直接积分。慢但是方便。
 
-def get_lw_spectrum(x, y, z, ux, uy, uz, t, n, omega_axis, use_cuda=False):
+    RE : ndarray
+        推迟势电场矢量REx, REy or REz
+    t_ret : ndarray
+        推迟时间矢量
+    omega_axis : ndarray
+        频谱的频率轴
+    '''
+    assert RE.ndim == 2 and t_ret.ndim == 2 and omega_axis.ndim == 1
+
+    nomega = len(omega_axis)
+    npart, nt = RE.shape
+
+    RE_ft = np.zeros((nomega, npart), dtype=np.complex128)
+    norm = 1 / np.sqrt(c*mu_0) / np.sqrt(2*pi)
+    
+    # definition of Fourier transformation
+    for i in prange(nomega*npart):
+        iw = i // npart
+        ip = i % npart
+        w = omega_axis[iw]
+        # trapzoid integral
+        RE_ft[iw, ip] = np.trapz(RE[ip] * np.exp(1j*w*t_ret[ip]), t_ret[ip]) * norm
+
+    # normalize
+    return RE_ft
+
+def get_lw_spectrum(
+    x, y, z, ux, uy, uz, t, n, omega_axis, 
+    use_cuda=False
+):
     '''
     从坐标和动量计算LW场的频谱
 
     Prameters
     ===
     x, y, z : ndarray
-        坐标向量
+        坐标向量或矩阵。其中矩阵的行为时间、列为不同粒子
     ux, uy, uz : ndarray
-        归一化动量，u = p/mc
+        归一化动量，u = p/mc。结构与xyz相同。
     t : ndarray
         时间矢量
     n : tuple | list | ndarray
@@ -131,15 +165,46 @@ def get_lw_spectrum(x, y, z, ux, uy, uz, t, n, omega_axis, use_cuda=False):
     I: ndarray
         返回dI/dΩdω
     '''
-    t_ret, REx, REy, REz = get_lw_RE(x, y, z, ux, uy, uz, t, n)
-    
+    ndim = x.ndim
+    nomega = len(omega_axis)
+    if use_cuda:
+        try:
+            from .gpu import get_RE_spectrum_d, get_RE_spectrum_2d_d
+        except ImportError as e:
+            raise e
 
-    I = np.zeros(len(omega_axis))
-    for RE in (REx, REy, REz):
-        if use_cuda:
-            RE_ft = get_RE_spectrum_d(RE, t_ret, omega_axis)
-        else:
-            RE_ft = get_RE_spectrum(RE, t_ret, omega_axis)
-        I += RE_ft.real**2 + RE_ft.imag**2
-    I *= 2
-    return I
+
+    if ndim == 1:
+        t_ret, REx, REy, REz = get_lw_RE(x, y, z, ux, uy, uz, t, n)
+        I = np.zeros(nomega)
+        for RE in (REx, REy, REz):
+            if use_cuda:
+                RE_ft = get_RE_spectrum_d(RE, t_ret, omega_axis)
+            else:
+                RE_ft = get_RE_spectrum(RE, t_ret, omega_axis)
+            I += RE_ft.real**2 + RE_ft.imag**2
+        I *= 2
+        return I
+    elif ndim == 2:
+        nt, npart = x.shape
+
+        t_ret = np.zeros((npart, nt-2))
+        REx = np.zeros((npart, nt-2))
+        REy = np.zeros((npart, nt-2))
+        REz = np.zeros((npart, nt-2))
+
+        for i in range(npart):
+            t_ret[i, :], REx[i, :], REy[i, :], REz[i, :] = get_lw_RE(x[:, i], y[:, i], z[:, i], ux[:, i], uy[:, i], uz[:, i], t, n)
+        
+
+        I = np.zeros((nomega, npart))
+        for RE in (REx, REy, REz):
+            if use_cuda:
+                RE_ft = get_RE_spectrum_2d_d(RE, t_ret, omega_axis)
+            else:
+                RE_ft = get_RE_spectrum_2d(RE, t_ret, omega_axis)
+            I += RE_ft.real**2 + RE_ft.imag**2
+        I *= 2
+        return I.sum(axis=1)
+    else:
+        raise TypeError("input x, y, z, ux, uy, uz must 1d vector or 2d array.")
--- lw/gpu.py
+++ lw/gpu.py
@@ -1,6 +1,5 @@
 import numpy as np
 from scipy.constants import pi, m_e, e, c, alpha, hbar, epsilon_0, mu_0
-from numba import njit, prange
 import cupy as cp
 
 def get_RE_spectrum_d(RE, t_ret, omega_axis):
@@ -20,5 +19,33 @@ def get_RE_spectrum_d(RE, t_ret, omega_axis):
     t_ret_d = cp.asarray(t_ret)
     omega_axis_d = cp.asarray(omega_axis)[:, None]
     RE_ft_d = cp.trapz(RE_d * cp.exp(1j*omega_axis_d*t_ret_d), t_ret_d, axis=1) * norm
+    
+    return RE_ft_d.get()
+
+def get_RE_spectrum_2d_d(RE, t_ret, omega_axis):
+    '''
+    计算lw谱，不使用FFT直接积分。慢但是方便。
+
+    RE : ndarray
+        推迟势电场矢量REx, REy or REz
+    t_ret : ndarray
+        推迟时间矢量
+    omega_axis : ndarray
+        频谱的频率轴
+    '''
+    assert RE.ndim == 2 and t_ret.ndim == 2 and omega_axis.ndim == 1
+    norm = 1 / np.sqrt(c*mu_0) / np.sqrt(2*pi)
+
+    nomega = len(omega_axis)
+    npart, nt = RE.shape
+
+    RE_d = cp.asarray(RE)
+    t_ret_d = cp.asarray(t_ret)
+    omega_axis_d = cp.asarray(omega_axis)[:, None]
+
+    RE_ft_d = cp.zeros((nomega, npart), dtype=np.complex128)
+    for iw in range(nomega):
+        w = omega_axis_d[iw]
+        RE_ft_d[iw, :] = cp.trapz(RE_d * cp.exp(1j*w*t_ret_d), t_ret_d, axis=1) * norm
     
     return RE_ft_d.get()
